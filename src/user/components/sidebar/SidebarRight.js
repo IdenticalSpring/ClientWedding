8cba7694b2c1607b00a7a3e0db11873a3f9c68fc
quangphucc0903 <91825763+quangphucc0903@users.noreply.github.com>
2025-01-07 00:05:40 +0700

    update font

--- src/user/components/sidebar/SidebarRight.js
+++ src/user/components/sidebar/SidebarRight.js
@@ -29,12 +29,36 @@ const SidebarRight = ({
     }
   };
 
+  const fontNames = [
+    { family: "Montserrat", label: "Montserrat" },
+    { family: "DancingScript", label: "Dancing Script" },
+    { family: "GreatVibes", label: "Great Vibes" },
+    { family: "AlexBrush", label: "Alex Brush" },
+    { family: "PlayfairDisplay", label: "Playfair Display" },
+    { family: "CormorantGaramond", label: "Cormorant Garamond" },
+    { family: "Quicksand", label: "Quicksand" },
+    { family: "PinyonScript", label: "Pinyon Script" },
+    { family: "Charm", label: "Charm" },
+    { family: "JosefinSans", label: "Josefin Sans" },
+    { family: "Raleway", label: "Raleway" },
+    { family: "Nunito", label: "Nunito" },
+    { family: "Roboto", label: "Roboto" },
+    { family: "Lora", label: "Lora" },
+    { family: "SourceSerifPro", label: "Source Serif Pro" },
+    { family: "Bellota", label: "Bellota" },
+    { family: "Philosopher", label: "Philosopher" },
+    { family: "AmaticSC", label: "Amatic SC" },
+    { family: "CormorantInfant", label: "Cormorant Infant" },
+    { family: "Bungee", label: "Bungee" },
+    { family: "Pacifico", label: "Pacifico" },
+  ]; 
+
   useEffect(() => {
     if (
       selectedComponent?.type === "text" &&
       selectedComponent.style?.fontFamily
     ) {
-      const currentFont = fonts.find(
+      const currentFont = fontNames.find(
         (font) => font.family === selectedComponent.style.fontFamily
       );
       if (currentFont) {
@@ -102,7 +126,7 @@ const SidebarRight = ({
             value={selectedFont}
             onChange={(e) => {
               const selectedFontFamily = e.target.value;
-              const selectedFontObj = fonts.find(
+              const selectedFontObj = fontNames.find(
                 (font) => font.family === selectedFontFamily
               );
               if (selectedFontObj) {
@@ -115,7 +139,7 @@ const SidebarRight = ({
             displayEmpty
             variant="outlined"
           >
-            {fonts.map((font) => (
+            {fontNames.map((font) => (
               <MenuItem
                 key={font.value}
                 value={font.family}
--- src/user/template/template-component/StyleEditor.js
+++ src/user/template/template-component/StyleEditor.js
@@ -90,10 +90,34 @@ const StyleEditor = ({
     }
   };
 
+  const fontNames = [
+    { family: "Montserrat", label: "Montserrat" },
+    { family: "DancingScript", label: "Dancing Script" },
+    { family: "GreatVibes", label: "Great Vibes" },
+    { family: "AlexBrush", label: "Alex Brush" },
+    { family: "PlayfairDisplay", label: "Playfair Display" },
+    { family: "CormorantGaramond", label: "Cormorant Garamond" },
+    { family: "Quicksand", label: "Quicksand" },
+    { family: "PinyonScript", label: "Pinyon Script" },
+    { family: "Charm", label: "Charm" },
+    { family: "JosefinSans", label: "Josefin Sans" },
+    { family: "Raleway", label: "Raleway" },
+    { family: "Nunito", label: "Nunito" },
+    { family: "Roboto", label: "Roboto" },
+    { family: "Lora", label: "Lora" },
+    { family: "SourceSerifPro", label: "Source Serif Pro" },
+    { family: "Bellota", label: "Bellota" },
+    { family: "Philosopher", label: "Philosopher" },
+    { family: "AmaticSC", label: "Amatic SC" },
+    { family: "CormorantInfant", label: "Cormorant Infant" },
+    { family: "Bungee", label: "Bungee" },
+    { family: "Pacifico", label: "Pacifico" },
+  ];  
+
   // Load the font if a font family is selected
   useEffect(() => {
     if (activeStyles?.fontFamily) {
-      const selectedFont = fonts.find(
+      const selectedFont = fontNames.find(
         (font) => font.family === activeStyles.fontFamily
       );
       if (selectedFont) {
@@ -167,7 +191,7 @@ const StyleEditor = ({
           value={activeStyles.fontFamily}
           onChange={(value) => handleStyleChange("fontFamily", value)}
           type="select"
-          options={fonts.map((font) => font.family)}
+          options={fontNames?.map((font) => font.family)}
         />
 
         <Typography variant="subtitle2" sx={{ fontWeight: 500, mt: 2 }}>
